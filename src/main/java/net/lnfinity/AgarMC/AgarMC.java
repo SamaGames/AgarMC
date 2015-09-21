3b1a9ced030a585154046b58d959bf441a34f910
Rigner <rigner@pecher.com>
2015-09-22 01:03:02 +0200

    Added WorldBoard round Map

--- src/main/java/net/lnfinity/AgarMC/AgarMC.java
+++ src/main/java/net/lnfinity/AgarMC/AgarMC.java
@@ -18,6 +18,7 @@ import net.samagames.api.network.IJoinManager;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -61,6 +62,9 @@ public class AgarMC extends JavaPlugin {
 			for (int j = 0; j < 7; j++)
 				this.getWorld().loadChunk(i, j);
 		game.initialize();
+		getWorld().getWorldBorder().setCenter(new Location(getWorld(), Game.DIMENSIONS / 2, 128, Game.DIMENSIONS / 2));
+		getWorld().getWorldBorder().setSize(Game.DIMENSIONS + 9D);
+		getWorld().getWorldBorder().setWarningDistance(0);
 		
 		SamaGamesAPI.get().getGameManager().registerGame(game);
 		
