b88c3f6449d123a2510c87af7f457e067040520b
BlueSlime <jeremylevilain78@orange.fr>
2017-08-23 19:21:43 +0200

    Publishing source code.

---
+++ README.md
@@ -0,0 +1,20 @@
+![SamaGames](https://assets.samagames.net/images/logo.png "SamaGames logo")
+
+## SamaGames - AgarMC
+
+Ce projet vous est mis a disposition dans une volonté de partage suite à la fermeture du serveur Minecraft [**SamaGames**](http://samagames.net).
+
+Ce logiciel est publié sous licence GNU GPL version 3. Vous pouvez télécharger, modifier ou redistribuer — même commercialement — ce logiciel librement, tant que vous *précisez les changements apportés* et que vous *redistribuez vos modifications sous la même licence* (avec le code source), sans oublier de *préciser que votre travail est une reprise de ce projet de SamaGames*.
+Pour plus de détails, référez-vous au texte de licence disponible [ici](LICENCE).
+
+------------------------------------
+
+- Nom : AgarMC
+- Type : Mini-Jeu
+- État : Terminé
+- Développeur(s) : Rigner, 6infinity8
+- CDC (Si existant) :
+
+
+## Description
+AgarMC est un projet visant à reproduire AgarIO dans Minecraft. 
--- src/main/java/net/lnfinity/AgarMC/events/WorldListener.java
+++ src/main/java/net/lnfinity/AgarMC/events/WorldListener.java
@@ -9,6 +9,22 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
+/*
+ * This file is part of AgarMC.
+ *
+ * AgarMC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AgarMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AgarMC.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class WorldListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
